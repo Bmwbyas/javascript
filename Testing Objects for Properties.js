e55b674d2b1e943355a654ac9d865f97fe4c805f
Alexandr <askerko50@gmail.com>
2022-05-18 20:41:22 +0300

    three days ago

---
+++ Testing Objects for Properties.js
@@ -0,0 +1,10 @@
+function checkObj(obj, checkProp) {
+    if(obj.hasOwnProperty(checkProp)==true){
+      return obj[checkProp];
+    } else{
+      return "Not Found";
+    }
+    // Only change code below this line
+    return "Change Me!";
+    // Only change code above this line
+  }
